f0ab2aeff44006ef5031d67ba0f1690617892d41
Kyoungeun JO <hke3259@naver.com>
2022-10-10 23:47:35 +0200

    Chore: setup Redux

---
+++ src/Reducers/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    isLoggedIn: false,
+    firstName: "",
+    lastName: ""
+  },
+  reducers: {
+    setProfile: (state, action) => {
+        state.firstName = action.payload.user.firstName
+        state.lastName = action.payload.user.lastName
+    },
+    logIn: (state) => {
+        state.isLoggedIn = true
+      },
+      logOut: (state) => {
+        state.isLoggedIn = false
+      }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setProfile, logIn, logOut } = userSlice.actions
+
+export default userSlice.reducer
